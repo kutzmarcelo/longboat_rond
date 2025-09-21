e5feb4e21fe4c33a4e7a891ffdc746c0964e7c50
Marcelo Kutz <kutzmarcelo@gmail.com>
2025-09-21 20:02:39 +0200

    background blur when menu opened

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,11 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
     const menuToggle = document.querySelector('.header__menu-toggle');
     const menu = document.querySelector('.header__menu');
+    const container = document.querySelector('.layout-container');
 
     if (menuToggle && menu) {
         menuToggle.addEventListener('click', function () {
             menu.classList.toggle('header__menu--open');
             menuToggle.classList.toggle('header__menu-toggle--active');
+            container.classList.toggle('container__menu--open');
         });
     }
 });
